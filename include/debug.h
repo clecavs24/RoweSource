82930c8d0d65ad4d9d2e1637a3e43e96cc4920a4
clecavs24 <mhoransky2@yahoo.com>
2024-05-11 21:08:20 -0400

    Update debug.h

--- include/debug.h
+++ include/debug.h
@@ -1,12 +1,8 @@
 #ifndef GUARD_DEBUG_H
 #define GUARD_DEBUG_H
+#if DEBUGGING
 
-#define DEBUG_MENU
-
-#ifdef DEBUG_MENU
 void Debug_ShowMainMenu(void);
+
 #endif
-
-void CB2_StartSoundCheckMenu(void);
-
 #endif // GUARD_DEBUG_H
